b17a4baceb086777843e2e226355817c7cd80352
Enital <117684114+Enital@users.noreply.github.com>
2023-08-13 12:05:11 +0300

    13-12.05

--- server.js
+++ server.js
@@ -4,23 +4,38 @@
 //   console.log("Server running. Use our API on port: 3000")
 // })
 
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 
-const DB_URI =
-  "mongodb+srv://admin:yh0RoMOMdCwK2F4q@cluster4me.4o8rowx.mongodb.net/";
+// const DB_URI =
+//   "mongodb+srv://admin:yh0RoMOMdCwK2F4q@cluster4me.4o8rowx.mongodb.net/";
 
-const client = new MongoClient(DB_URI);
+// const client = new MongoClient(DB_URI);
 
-async function main() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } catch (error) {
-    console.error(error);
-  } finally {
-    await client.close();
-  }
-}
+// async function main() {
+//   try {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected successfully to server");
+//   } catch (error) {
+//     console.error(error);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-main();
+// main();
+// require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const DB_URI = process.env.DB_URI;
+//   "mongodb+srv://admin:yh0RoMOMdCwK2F4q@cluster4me.4o8rowx.mongodb.net/";
+
+mongoose
+  .connect(DB_URI)
+  .then(() => {
+    console.log("Connected successfully to Data Base");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
